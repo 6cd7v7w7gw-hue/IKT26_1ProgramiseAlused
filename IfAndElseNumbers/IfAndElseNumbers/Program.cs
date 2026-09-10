d4aff8f8de205d39cfc3876d350c68b641c54807
6cd7v7w7gw-hue <6cd7v7w7gw@privaterelay.appleid.com>
2026-09-10 10:51:43 +0300

    If and else numbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            // kasutada if ja else lauseid
+            string number = Console.ReadLine()
+                                   .Replace('.',',');
+
+            //kontrollime, kas sisestatud väärtus on number
+            if (float.TryParse(number, out float num))
+            {
+                Console.WriteLine("On number");
+            }
+            else
+            {
+                Console.WriteLine("Ei ole number");
+            }
+        }
+    }
+}

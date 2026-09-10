4bdb9f7ee894ea8d4bea0f825267bfc33fb59d97
6cd7v7w7gw-hue <6cd7v7w7gw@privaterelay.appleid.com>
2026-09-10 11:28:24 +0300

    If else nestimine

---
+++ IfAndElseNestimine/IfAndElseNestimine/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IfAndElseNestimine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If and else nesting");
+            // nesting tähenbise on see, et if-else struktuuride sees võib olla teisi
+            //if-else sktruktuure. See võimaldab teha keerukamaid otsuseid ja kontrollida
+            //erinevaid tingimusi
+
+            int number = 21;
+            if (number >= 9 && number < 21)
+            {
+                //kui sisestan 9, siis see if-else sktruktuur käivitub
+                if (number == 11)
+                {
+                    Console.WriteLine("number on 11");
+                }
+                else
+                {
+                    Console.WriteLine("vastus oli 0 kuni 10, ja 12 kuni 19");
+                }
+            }
+            else if (number == 21)
+            {
+                Console.WriteLine("vastus oli 21");
+            }
+            else if (number == 30)
+            {
+                Console.WriteLine("vastus oli 30");
+            }
+            else
+            {
+                Console.WriteLine("mingi kahtlane number");
+            }
+        }
+    }
+}

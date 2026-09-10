484545051786385e1da487322e9c0a0ae78c8481
6cd7v7w7gw-hue <6cd7v7w7gw@privaterelay.appleid.com>
2026-09-10 11:35:33 +0300

    if else nesting 2

---
+++ IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 30;
+
+            if (number > 5)
+            {
+                if (number == 6)
+                {
+                    Console.WriteLine("number on 6");
+                }
+                else if (number >= 12)
+                {
+                    if (number == 30)
+                    {
+                        Console.WriteLine("number on 30");
+                    }
+                    else
+                    {
+                        Console.WriteLine("number on suurem kui 12");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("number on suurem kui 5");
+                }
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("number on 3");
+            }
+            else
+            {
+                Console.WriteLine("number on väiksem kui 5 ja ei ole 3");
+            }
+        }
+    }
+}

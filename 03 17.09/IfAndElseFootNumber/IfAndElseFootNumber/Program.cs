b9f9cda04f1dd95f7bf4b15dd76b7c1a7462de5c
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 11:08:53 +0300

    IfAndElseFootNum

---
+++ 03 17.09/IfAndElseFootNumber/IfAndElseFootNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace IfAndElseFootNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jalanumber");
+
+            int jn = 0;
+
+            if (int.TryParse(Console.ReadLine(), out int b))
+            {
+                jn = b;
+            }
+
+            if (jn > 30 && jn <= 33)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (jn > 34 && jn <= 38)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+            else if (jn > 39 && jn <= 44)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+            else if (jn > 45 && jn <= 48)
+            {
+                Console.Beep();
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine("Sinu jalanumber on " + jn);
+
+
+
+
+        }
+    }
+}

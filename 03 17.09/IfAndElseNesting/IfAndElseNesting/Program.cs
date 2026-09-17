731e0edb8c34a5ffeab28c2a1fbbc02ca185cb51
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 10:44:02 +0300

    IfAndElseAge

---
+++ 03 17.09/IfAndElseNesting/IfAndElseNesting/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            int a = 0;
+            if (int.TryParse(Console.ReadLine(), out int b))
+            {
+                a = b;
+            }
+            if (a == 12)
+            {
+                Console.WriteLine("a = 12");
+            }
+            else if (a > 20)
+            {
+                if (a > 30)
+                {
+                    Console.WriteLine("a > 30");
+                }
+                else
+                {
+                    Console.WriteLine("a > 20 ja < 30");
+                }
+            }
+            else
+            {
+                Console.WriteLine("minigt kahtlased väärtused");
+            }
+        }
+    }
+}

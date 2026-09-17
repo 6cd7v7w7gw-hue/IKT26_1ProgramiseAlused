b4074942b57b0d8582feb014224218806fe1593d
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 11:36:47 +0300

    Update Program.cs

--- 03 17.09/IfAndElseFootNumber/IfAndElseFootNumber/Program.cs
+++ 03 17.09/IfAndElseFootNumber/IfAndElseFootNumber/Program.cs
@@ -13,28 +13,32 @@
                 jn = b;
             }
 
-            if (jn > 30 && jn <= 33)
+            if (jn >= 30 && jn <= 33)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jalanumber on 30 kuni 33");
             }
-            else if (jn > 34 && jn <= 38)
+            else if (jn >= 34 && jn <= 38)
             {
                 Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("Jalanumber on 34 kuni 38");
             }
-            else if (jn > 39 && jn <= 44)
+            else if (jn >= 39 && jn <= 44)
             {
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
                 Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("Jalanumber on 39 kuni 44");
             }
-            else if (jn > 45 && jn <= 48)
+            else if (jn >= 45 && jn <= 48)
             {
+                Console.WriteLine("Jalanumber on 45 kuni 48");
                 Console.Beep();
             }
             else
             {
                 Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sinu jalanumber on liiga suur voi vaike");
             }
-            Console.WriteLine("Sinu jalanumber on " + jn);
         }
     }
 }

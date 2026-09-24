0d3ffd62d6fb694c79856ca8140bb2c5db9638f6
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 10:53:41 +0300

    24.09.26

---
+++ 04 24.09/IfElseMethodCall/IfElseMethodCall/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IfElseMethodCall
+{
+    internal class Program
+    {
+        // Main on meetod, mis läheb alati esimesena tööle
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("kas soovid meetod kutsuda");
+            if (Console.ReadLine() == "jah")
+            {
+                //kui kirjutan meetodi nime, siis seda nimetatakse
+                //meetodi välja kutsumiseks
+                HelloMethod();
+            }
+            else
+            {
+                Console.WriteLine("siis ei kutse");
+            }
+        }
+
+        //Teha teine meetod siia ja nimeks on sellel HelloMethod
+
+        static void HelloMethod()
+        {
+            Console.WriteLine("hello kitty");
+        }
+    }
+}
